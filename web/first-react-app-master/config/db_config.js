3f6945b697419d3c644d94bf7e114d920b6d54f3
suhyunyoon <billy555@naver.com>
2020-12-11 14:58:54 +0900

    rename directories

--- web/first-react-app-master/config/db_config.js
+++
@@ -1,9 +0,0 @@
-// Set by yourself
-module.exports = {
-	host	: 'localhost',
-	user	: 'username',
-	password: 'password',
-	database: 'database',
-	insecureAuth: true
-};
-
--- web/portfolio-nodejs-tutorial-master/README.md
+++
@@ -1,5 +0,0 @@
-# portfolio-nodejs-tutorial
-for Educational
-
-npm install
-npm start 하면 됨
