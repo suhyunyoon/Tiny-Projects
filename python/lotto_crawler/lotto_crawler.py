58c82e98f4a2b7297c42582e7a1380e05c54e5ff
suhyunyoon <billy555@naver.com>
2021-06-10 22:15:17 +0900

    Get newest number, Record all balls

--- python/lotto_crawler/lotto_crawler.py
+++ python/lotto_crawler/lotto_crawler.py
@@ -5,21 +5,27 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 #from tqdm import tqdm_notebook
 
+# Get newest lotto draw number
+source = requests.get("https://dhlottery.co.kr/gameResult.do?method=byWin&drwNo=").text
+soup = BeautifulSoup(source, "html.parser")
+newest = int(soup.select("select#dwrNoList option")[0].text)
+
+# initialize arrays
 total = 45
-newest = 930
-freq = np.array([0] * total)
+records = np.zeros((newest, total))
 
 print('Crawling...')
-for i in tqdm(range(1, 30)):
+for i in tqdm(range(newest, 0, -1)):
+    # parsing
     source = requests.get("https://dhlottery.co.kr/gameResult.do?method=byWin&drwNo=" + str(i)).text
     soup = BeautifulSoup(source, "html.parser")
     hotKeys = soup.select("span.ball_645")
+    
+    # count ith balls
+    balls = list(map(lambda x: int(x.text)-1, hotKeys))
+    records[i-1, balls] += 1
 
-    for key in hotKeys:
-        num = int(key.text)
-        freq[num-1] += 1
-
-hist = plt.bar(np.arange(45)+1, freq, color='r')
+hist = plt.bar(np.arange(45)+1, np.sum(records, axis=0), color='r')
 plt.title('The number of lotto numbers')
 plt.xticks(np.arange(45)+1)
 plt.show()
